c64694d821adbe8e6c40d9cc047262247d9c49ab
Talari Nithin <150463959+talarinithin@users.noreply.github.com>
2024-06-02 15:41:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let Text = document.getElementById('text');
+const sr=ScrollReveal({
+    distance:'65px',
+    duration:2600,
+    delay:450,
+    reset:true
+});
+
+sr.reveal('.h-text',{delay:300,origin:'top'});
+sr.reveal('.icons',{delay:300,origin:'left'});
